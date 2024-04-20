7346231423cfc3b48ca2158a149657f1c8aeee2b
SanyaMar <125236859+SanyaMar@users.noreply.github.com>
2024-04-20 17:59:16 +0400

    added the design of the pep8

--- lab_1/main.py
+++ lab_1/main.py
@@ -2,9 +2,18 @@ import json
 
 from cons import file_path
 
-def read_text_from_file(file_path):
+
+def read_text_from_file(file_path: str) -> str:
+    """
+    This function reads the file format .txt
+
+    Arguments:
+        file_path: location the .txt file whose contents you want to find out
+    Returns:
+        text: content .txt file
+    """
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             text = file.read()
             return text
     except FileNotFoundError:
@@ -12,18 +21,34 @@ def read_text_from_file(file_path):
     except Exception as e:
         print(f"Произошла ошибка: {e}")
 
-def write_to_txt_file(text, file_name):
+
+def write_to_txt_file(text: str, file_name: str) -> None:
+    """
+    This function writes data to a .txt file
+
+    Arguments:
+        text: the data that we write to the file
+        file_name: location the .txt file in which we write the data
+    """
     try:
-        with open(file_name + '.txt', 'w',encoding='utf-8') as file:
+        with open(file_name + ".txt", "w", encoding="utf-8") as file:
             file.write(text)
         print(f"Текст успешно записан в файл {file_name}.txt")
     except Exception as e:
         print(f"Произошла ошибка при записи в файл: {e}")
 
 
-def read_json_file(file_name):
+def read_json_file(file_name: str) -> dict[str, str]:
+    """
+    This function reads the file format .json
+
+    Arguments:
+        file_name: location the .json file whose contents you want to find out
+    Returns:
+        data: dictionary of keys
+    """
     try:
-        with open(file_name, 'r', encoding='utf-8') as file:
+        with open(file_name, "r", encoding="utf-8") as file:
             data = json.load(file)
         return data
     except Exception as e:
@@ -31,19 +56,36 @@ def read_json_file(file_name):
         return None
 
 
-def write_json_file(data, file_name):
+def write_json_file(data: str, file_name: str) -> None:
+    """
+    This function writes data to a .json file
+
+    Arguments:
+        data: the data that we write to the file
+        file_name: location the .json file in which we write the data
+    """
     try:
-        with open(file_name, 'w',  encoding='utf-8') as file:
+        with open(file_name, "w", encoding="utf-8") as file:
             json.dump(data, file, indent=4, ensure_ascii=False)
         print(f"Данные успешно записаны в файл {file_name}")
     except Exception as e:
         print(f"Произошла ошибка при записи файла: {e}")
 
-def character_frequency_analysis(file_name, json_name):
+
+def character_frequency_analysis(file_name: str, json_name: str) -> dict[str, str]:
+    """
+    This function performs a frequency analysis of the received text and outputs the frequency index of the character
+
+    Arguments:
+        file_name: the location of the file in which the text under study is located
+        json_name: the location of the file where the analysis result is recorded
+    Returns:
+        dict: the dictionary in which the characters and their frequency are recorded
+    """
     frequency = {}
-    text=read_text_from_file(file_name)
-    total_chars = len(text)  
-    
+    text = read_text_from_file(file_name)
+    total_chars = len(text)
+
     for char in text:
         if char in frequency:
             frequency[char] += 1
@@ -58,11 +100,23 @@ def character_frequency_analysis(file_name, json_name):
 
     # for char, freq in sorted_freq.items():
     #     print(f" '{char}':  {freq:.6f}")
+
+
 # character_frequency_analysis("lab_1/task2/text2.txt","lab_1/task2/frequency_analysis.json")
 
-def encrypt(name_file, key_file):
+
+def encrypt(name_file: str, key_file: str) -> str:
+    """
+    This function encrypts the specified text using a key
+
+    Arguments:
+        name_file: the location of the file in which the text is to be encrypted
+        key_file: the location of the file where the encryption key is recorded
+    Returns:
+        encrypted_text: key-encrypted text
+    """
     try:
-        key=read_json_file(key_file)
+        key = read_json_file(key_file)
         text = read_text_from_file(name_file)
         encrypted_text = ""
         for char in text:
@@ -71,10 +125,20 @@ def encrypt(name_file, key_file):
     except Exception as e:
         print(f"Произошла ошибка при шифровании: {e}")
         return None
-    
-def decrypt_text_with_key(name, key_file):
+
+
+def decrypt_text_with_key(name: str, key_file: str) -> str:
+    """
+    This function decrypts the specified text using a key
+
+    Arguments:
+        name: the location of the file in which the text is to be decrypted
+        key_file: the location of the file where the decryption key is recorded
+    Returns:
+        encrypted_text: key-decrypted text
+    """
     try:
-        key=read_json_file(key_file)
+        key = read_json_file(key_file)
         text = read_text_from_file(name)
         decrypted_text = ""
         for char in text:
@@ -86,17 +150,17 @@ def decrypt_text_with_key(name, key_file):
 
 
 def main() -> None:
-    path_of_file=read_json_file(file_path)
-    text1=path_of_file["path_text_1"]
-    key1=path_of_file["path_key_1"]
-    encrypted_text=path_of_file["path_encrypted_text"]
-    encrypted=encrypt(text1,key1)
-    write_to_txt_file(encrypted,encrypted_text)
-    text2=path_of_file["path_taxt_2"]
-    key2=path_of_file["path_key_2"]
-    decrupted_text=path_of_file["path_decrupted_text"]
-    text = decrypt_text_with_key(text2,key2)
-    write_to_txt_file(text,decrupted_text)
+    path_of_file = read_json_file(file_path)
+    text1 = path_of_file["path_text_1"]
+    key1 = path_of_file["path_key_1"]
+    encrypted_text = path_of_file["path_encrypted_text"]
+    encrypted = encrypt(text1, key1)
+    write_to_txt_file(encrypted, encrypted_text)
+    text2 = path_of_file["path_taxt_2"]
+    key2 = path_of_file["path_key_2"]
+    decrupted_text = path_of_file["path_decrupted_text"]
+    text = decrypt_text_with_key(text2, key2)
+    write_to_txt_file(text, decrupted_text)
 
 
 if __name__ == "__main__":
