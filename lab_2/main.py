8fa4a33ea288f35513162e23f6875da7d18f7188
SanyaMar <125236859+SanyaMar@users.noreply.github.com>
2024-05-08 22:51:45 +0400

    Second test

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,6 +1,5 @@
 import json
 import math
-import mpmath
 
 
 def read_json_file(file_name: str) -> dict[str, str]:
@@ -37,18 +36,26 @@ def bit_frequency_test(sequence):
         return math.erfc(sn / math.sqrt(2))
     except Exception as e:
         print(f"Произошла ошибка во время теста: {e}")
-# print(bit_frequency_test("lab_2/sequences.json"))
-# def frequency_bitwise_test(name: str) -> float:
-#     """
-#  The function checks whether the sequence is random enough
-#  Args:
-#  name: path .json file
-#  Returns:
-#  float: P-value is the probability that the
-#  generator produces values comparable
-#  to the reference
-#     """
-#     s = abs(sum(1 if i == "1" else -1 for i in name)) / math.sqrt(len(name))
-#     return math.erfc(s / math.sqrt(2))
+
+
+def ident_con_bits_test(E):
+    """
+    
+    """
+    try:
+        n = len(E)
+        s=(E.count("1")/n)
+        if abs(s-0.5)>=(2/math.sqrt(n)):
+            return
+        vn = 0
+        for i in range(0, n - 1):
+            if E[i] != E[i + 1]:
+                vn += 1
+        pv = math.erfc(abs(vn - 2 * n * s * (1 - s))/ (2 * math.sqrt(2*n)* s * (1 - s)))
+        return pv
+    except Exception as e:
+            print(f"Произошла ошибка во время теста: {e}")
+
+
 name="01110010110011101001001101011001011010011101100100001110111001100101110011011010111000100000001110001110100110001010010100000100"
-print(bit_frequency_test(name))
+print(ident_con_bits_test(name))
