a2d2b77b88c053bc52e2111fc68497b57e593958
SanyaMar <125236859+SanyaMar@users.noreply.github.com>
2024-05-11 13:46:46 +0400

    Third test

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,6 +1,8 @@
 import json
 import math
+import mpmath
 
+from const import M, PI
 
 def read_json_file(file_name: str) -> dict[str, str]:
     """
@@ -54,8 +56,47 @@ def ident_con_bits_test(E):
         pv = math.erfc(abs(vn - 2 * n * s * (1 - s))/ (2 * math.sqrt(2*n)* s * (1 - s)))
         return pv
     except Exception as e:
-            print(f"Произошла ошибка во время теста: {e}")
+        print(f"Произошла ошибка во время теста: {e}")
 
+
+def find_max_consecutive_ones(block):
+    max_ones = 0
+    current_ones = 0
+    
+    for bit in block:
+        if bit == '1':
+            current_ones += 1
+            max_ones = max(max_ones, current_ones)
+        else:
+            current_ones = 0
+    
+    return max_ones
+def unit_sequence_test(E):
+    """
+    
+    """
+    try:
+        n = len(E)
+        observed_freq = [0, 0, 0, 0]
+        for i in range(0,n,M):
+            block = E[i : (i+M)]
+            max_ones = find_max_consecutive_ones(block)
+            
+            if max_ones <= 1:
+                observed_freq[0] += 1
+            elif max_ones == 2:
+                observed_freq[1] += 1
+            elif max_ones == 3:
+                observed_freq[2] += 1
+            else:
+                observed_freq[3] += 1
+        x_squary=0
+        for i in range(4):
+            x_squary+=((observed_freq[i]-16*PI[i])**2)/(16*PI[i])
+        pv=mpmath.gammainc(3/2, x_squary/2)
+        return pv
+    except Exception as e:
+        print(f"Произошла ошибка во время теста: {e}")
 
 name="01110010110011101001001101011001011010011101100100001110111001100101110011011010111000100000001110001110100110001010010100000100"
-print(ident_con_bits_test(name))
+print(unit_sequence_test(name))
