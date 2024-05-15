55ca028cea0ebf8bf275af154c6802f6f5b22d0a
SanyaMar <125236859+SanyaMar@users.noreply.github.com>
2024-05-15 21:36:57 +0400

    Redoing

--- lab_2/const.py
+++ lab_2/const.py
@@ -1,3 +1,4 @@
-M=8
+M = 8
 PI = [0.2148, 0.3672, 0.2305, 0.1875]
-PATH="lab_2/sequences.json"
+PATH = "lab_2/sequences.json"
+ 
--- lab_2/main.py
+++ lab_2/main.py
@@ -2,15 +2,17 @@ import json
 import math
 import mpmath
 
-from const import M, PI,PATH
+from const import M, PI, PATH
+
 
 def read_json_file(file_name: str) -> dict[str, str]:
     """
- This function reads the file format .json
- Arguments:
- file_name: location the .json file whose contents you want to find out
- Returns:
- data: dictionary of keys
+    This function reads the file format .json
+
+    Arguments:
+                file_name: location the .json file whose contents you want to find out
+    Returns:
+                data: dictionary of keys
     """
     try:
         with open(file_name, "r", encoding="utf-8") as file:
@@ -19,60 +21,71 @@ def read_json_file(file_name: str) -> dict[str, str]:
     except Exception as e:
         print(f"Произошла ошибка при чтении файла: {e}")
         return None
-    
 
-def bit_frequency_test(e: str)-> float:
+
+def bit_frequency_test(e: str) -> float:
     """
- The function checks the sequence for randomness
- Arguments:
- sequence string
- Returns:
- float: P value of the generator 
+    The function checks the sequence for randomness
+    
+    Arguments:
+                e: sequence string
+    Returns:
+                float: P value of the sequences
     """
     try:
         n = len(e)
-        s=0
+        s = 0
         for e in e:
-            if e=="1":
-                xi=1
+            if e == "1":
+                xi = 1
             else:
-                xi=-1
-            s+=xi
-        sn=abs(s/(math.sqrt(n)))
+                xi = -1
+            s += xi
+        sn = abs(s / (math.sqrt(n)))
         return math.erfc(sn / math.sqrt(2))
     except Exception as e:
         print(f"Произошла ошибка во время теста: {e}")
 
 
-def ident_con_bits_test(e: str)->float:
+def ident_con_bits_test(e: str) -> float:
     """
     The function searches for sequences of all the same bits
- Arguments:
- sequence string
- Returns:
- float: P value of the generator 
+    
+    Arguments:
+                e: sequence string
+    Returns:
+                float: P value of the sequences
     """
     try:
         n = len(e)
-        s=(e.count("1")/n)
-        if abs(s-0.5)>=(2/math.sqrt(n)):
+        s = e.count("1") / n
+        if abs(s - 0.5) >= (2 / math.sqrt(n)):
             return
         vn = 0
         for i in range(0, n - 1):
             if e[i] != e[i + 1]:
                 vn += 1
-        pv = math.erfc(abs(vn - 2 * n * s * (1 - s))/ (2 * math.sqrt(2*n)* s * (1 - s)))
+        pv = math.erfc(
+            abs(vn - 2 * n * s * (1 - s)) / (2 * math.sqrt(2 * n) * s * (1 - s))
+        )
         return pv
     except Exception as e:
         print(f"Произошла ошибка во время теста: {e}")
 
 
-def find_max_consecutive_ones(block:str)-> int:
+def find_max_consecutive_ones(block: str) -> int:
+    """
+    The function finds the maximum number of consecutive units
+    
+    Arguments:
+                block:  sequence block
+    Returns:
+                int: the maximum number of consecutive units
+    """
     max_ones = 0
     current_ones = 0
-    
     for bit in block:
-        if bit == '1':
+        if bit == "1":
             current_ones += 1
             max_ones = max(max_ones, current_ones)
         else:
@@ -80,38 +93,44 @@ def find_max_consecutive_ones(block:str)-> int:
     return max_ones
 
 
-def unit_sequence_test(e:str)->float:
+def unit_sequence_test(e: str) -> float:
     """
+    The function searches for the longest sequence of units in a block
     
+    Arguments:
+                e: sequence string
+    Returns:
+                float: P value of the sequences
     """
     try:
         n = len(e)
         observed_freq = [0, 0, 0, 0]
-        for i in range(0,n,M):
-            block = e[i : (i+M)]
+        for i in range(0, n, M):
+            block = e[i : (i + M)]
             max_ones = find_max_consecutive_ones(block)
-            
-            if max_ones <= 1:
-                observed_freq[0] += 1
-            elif max_ones == 2:
-                observed_freq[1] += 1
-            elif max_ones == 3:
-                observed_freq[2] += 1
-            else:
-                observed_freq[3] += 1
-        x_squary=0
+
+            match max_ones:
+                case _ if max_ones <= 1:
+                    observed_freq[0] += 1
+                case 2:
+                    observed_freq[1] += 1
+                case 3:
+                    observed_freq[2] += 1
+                case _:
+                    observed_freq[3] += 1
+        x_squary = 0
         for i in range(4):
-            x_squary+=((observed_freq[i]-16*PI[i])**2)/(16*PI[i])
-        pv=mpmath.gammainc(3/2, x_squary/2)
+            x_squary += ((observed_freq[i] - 16 * PI[i]) ** 2) / (16 * PI[i])
+        pv = mpmath.gammainc(3 / 2, x_squary / 2)
         return pv
     except Exception as e:
         print(f"Произошла ошибка во время теста: {e}")
 
 
-def main()->None:
-    path_c=read_json_file(PATH)
-    seq_cpp=path_c["cpp"]
-    seq_java=path_c["java"]
+def main() -> None:
+    path_c = read_json_file(PATH)
+    seq_cpp = path_c["cpp"]
+    seq_java = path_c["java"]
     print("Результаты тестов: ")
     print(bit_frequency_test(seq_cpp))
     print(bit_frequency_test(seq_java))
