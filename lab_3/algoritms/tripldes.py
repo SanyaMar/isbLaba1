44f6086d372c787c71cc207df1af3533303e5491
SanyaMar <125236859+SanyaMar@users.noreply.github.com>
2024-06-03 13:54:16 +0400

    formatting tripldes

--- lab_3/algoritms/tripldes.py
+++ lab_3/algoritms/tripldes.py
@@ -7,12 +7,19 @@ from files_funct import (
     read_bytes_from_file,
     write_bytes_to_file,
     write_to_txt_file,
-    deserial_sym_key
+    deserial_sym_key,
 )
 
 
 class SymmetricKey:
-    def selecting_key_len(self):
+    """
+    A class for working with symmetric keys.
+    """
+
+    def selecting_key_len(self) -> int:
+        """
+        Requests the user to enter the key length and checks it for correctness.
+        """
         while True:
             key_len = int(input("Введите длину ключа ( 64, 128, или 192): "))
             if key_len == 64 or key_len == 128 or key_len == 192:
@@ -21,12 +28,18 @@ class SymmetricKey:
                 print("Неверный ввод. Пожалуйста, попробуйте снова.")
         return key_len
 
-    def generate_key(self, key_len):
-        key = os.urandom(key_len//8)
+    def generate_key(self, key_len: int) -> bytes:
+        """
+        Generates a symmetric key of a specified length.
+        """
+        key = os.urandom(key_len // 8)
         return key
 
-    def encrypt_text(self, path_text, path_en_text, path_key):
-        text = bytes(read_text_from_file(path_text),"UTF-8")
+    def encrypt_text(self, path_text: str, path_en_text: str, path_key: str) -> None:
+        """
+        Encrypts text using a symmetric encryption algorithm.
+        """
+        text = bytes(read_text_from_file(path_text), "UTF-8")
         key = deserial_sym_key(path_key)
 
         padder = padding.PKCS7(64).padder()
@@ -37,15 +50,20 @@ class SymmetricKey:
         encrypt_text = iv + encryptor.update(padded_data) + encryptor.finalize()
         write_bytes_to_file(path_en_text, encrypt_text)
 
-    def decrypt_text(self, path_en_text, path_dec_text, path_key):
+    def decrypt_text(
+        self, path_en_text: str, path_dec_text: str, path_key: str
+    ) -> None:
+        """
+        Decrypts the text using a symmetric encryption algorithm.
+        """
         encrypt_text = read_bytes_from_file(path_en_text)
-        key = deserial_sym_key(path_key) 
+        key = deserial_sym_key(path_key)
         iv = encrypt_text[:8]
         cipher = Cipher(algorithms.TripleDES(key), modes.CBC(iv))
         decryptor = cipher.decryptor()
         dc_text = decryptor.update(encrypt_text[8:]) + decryptor.finalize()
 
-
         unpadder = padding.PKCS7(64).unpadder()
         unpadded_dc_text = unpadder.update(dc_text) + unpadder.finalize()
         write_to_txt_file(unpadded_dc_text.decode("UTF-8"), path_dec_text)
+ 
